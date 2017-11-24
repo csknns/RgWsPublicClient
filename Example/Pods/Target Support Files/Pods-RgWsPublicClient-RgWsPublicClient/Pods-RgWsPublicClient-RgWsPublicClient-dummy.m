d664f5d6511c90169413f0aed3c423fe9da5c45f
Christos Koninis <christos.koninis@gmail.com>
2017-11-24 18:11:21 +0200

    Updating Podfile of Example project to Cocoapods 1.3.1 format. Increased min supported iOS version to 8.0

--- Example/Pods/Target Support Files/Pods-RgWsPublicClient-RgWsPublicClient/Pods-RgWsPublicClient-RgWsPublicClient-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RgWsPublicClient_RgWsPublicClient : NSObject
-@end
-@implementation PodsDummy_Pods_RgWsPublicClient_RgWsPublicClient
-@end
