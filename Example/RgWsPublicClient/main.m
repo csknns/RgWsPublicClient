d4dc17c6403b47c0dfd4dff7be42e0b2b31f1d99
Christos Koninis <christos.koninis@gmail.com>
2018-02-14 19:21:14 +0200

    adding dummy app delegate for tests so that code coverage stats will be unaffected adding simple test for running client with no username/password configuration

--- Example/RgWsPublicClient/main.m
+++ Example/RgWsPublicClient/main.m
@@ -7,12 +7,47 @@
 //
 
 #import <UIKit/UIKit.h>
-
 #import "CKNAppDelegate.h"
+
+/**
+ A empty app delegate to be used when running unit tests in order to avoid polluting the code coverage statistics
+ with code not runned directly from the test methods
+ */
+@interface TestAppDelegate : UIResponder <UIApplicationDelegate>
+@end
+
+@implementation TestAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+}
+
+@end
 
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
+#ifdef DEBUG
+        if (NSClassFromString(@"XCTestCase")) {
+            return UIApplicationMain(argc, argv, nil, NSStringFromClass([TestAppDelegate class]));
+        }
+#endif
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([CKNAppDelegate class]));
     }
 }
